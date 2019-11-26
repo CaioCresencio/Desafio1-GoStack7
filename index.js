9f6e7b81b6c154a6bf74dac684c6b9b9a2dd446d
CaioCresencio <caiocresencio98@gmail.com>
2019-11-26 11:22:00 -0300

    Operações de post, get e put

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
-server.get("/", (req,res) =>{
-    return res.json({message:'Hello World'})
-})
+const projects = [];
+
+server.get("/", (req, res) => {
+    return res.json({ message: 'Hello World' })
+});
+
+server.post('/projects', (req, res) => {
+    const data = req.body;
+    projects.push(data)
+    return res.json(data);
+});
+
+server.get('/projects', (req, res) => {
+    return res.json(projects);
+});
+
+server.put('/projects/:id', (req, res) => {
+    const id = req.params;
+    const { title } = req.body;
+
+    const project = projects.map(el => {
+       if(el.id == id){
+           return el;
+       }
+    });
+    project.title = title;
+    return res.json(project.title)
+});
 
 server.listen(3000);
